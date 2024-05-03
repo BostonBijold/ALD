ff0d88bcc64771a7d8bfef33b8257124dc0c1ff9
Boston Bijold <bostonrbijold@gmail.com>
2024-05-02 22:06:48 -0600

    Added a connection to my local mongodb database. Added a simple menu, added enter todo function.

--- main.py
+++ main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+import datetime
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import pymongo
+from datetime import date
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["ALD"]
+mycol = mydb["todos"]
+
+
+def enterTodo():
+    todo_title = input("what do you need to do today(title)? ")
+    todo_details = input("what do you need to do to complete it? ")
+    todo_date = datetime.datetime.now()
+
+    todo_deadline = datetime.datetime(year=todo_date.year, month=todo_date.month, day=todo_date.day, hour=23, minute=59).strftime("%Y-%m-%d %H:%M:%S")
+    todo_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    todo_complete = False
+
+    new_todo = {
+        "tile": todo_title,
+        "details": todo_details,
+        "date": todo_date,
+        "complete": todo_complete,
+        "deadline": todo_deadline
+    }
+    print(new_todo)
+
+
+
+def main():
+    while True:
+        x = int(input("enter"))
+        if x == 1:
+            enterTodo()
+
+        if x == 2:
+            break
+
+
+if __name__ == "__main__":
+    main()
